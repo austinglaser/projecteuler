2ba091735879354af359d475fd3adf976ef30619
Austin Glaser <austin.glaser@gmail.com>
2016-02-22 23:47:40 -0700

    be clever like whatsisface

--- p1.py
+++ p1.py
@@ -10,13 +10,18 @@ import docopt
 
 import numpy as np
 
-def sum_three_five_multiples_below(limit):
-    three_multiples = np.arange(3, limit, step=3)
-    five_multiples = np.arange(5, limit, step=5)
-    threes_sum = three_multiples.sum()
-    fives_sum = five_multiples.sum()
-    total_sum = threes_sum + fives_sum
+def sum_multiples_below(limit, factors=[3, 5]):
+    total_sum = 0
+    for factor in factors:
+        top = limit / factor if limit % factor != 0 else limit / factor - 1
+        pair_value = top + 1
+        n_pairs = top / 2
+        pair_sum = pair_value * n_pairs
+        center_value = top / 2 + 1 if top % 2 != 0 else 0
+        factor_sum = factor * (pair_sum + center_value)
+        total_sum += factor_sum
     return total_sum
+
 
 if __name__ == "__main__":
     args = docopt.docopt(__doc__)
@@ -26,4 +31,4 @@ if __name__ == "__main__":
     except:
         raise docopt.DocoptExit("<limit> must be an integer")
 
-    print sum_three_five_multiples_below(limit)
+    print sum_multiples_below(limit)
