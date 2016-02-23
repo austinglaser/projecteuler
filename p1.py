7a2143d17e657aa0fdcf8abfef3aa15713b85bb1
Austin Glaser <austin.glaser@gmail.com>
2016-02-22 23:56:34 -0700

    don't be too clever for you own good

--- p1.py
+++ p1.py
@@ -12,14 +12,11 @@ import numpy as np
 
 def sum_multiples_below(limit, factors=[3, 5]):
     total_sum = 0
-    for factor in factors:
-        top = limit / factor if limit % factor != 0 else limit / factor - 1
-        pair_value = top + 1
-        n_pairs = top / 2
-        pair_sum = pair_value * n_pairs
-        center_value = top / 2 + 1 if top % 2 != 0 else 0
-        factor_sum = factor * (pair_sum + center_value)
-        total_sum += factor_sum
+    for n in range(limit):
+        for factor in factors:
+            if n % factor == 0:
+                total_sum += n
+                break
     return total_sum
 
 
