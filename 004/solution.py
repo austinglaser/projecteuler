d73f2f92fbcef4285618ff47acda31a59b91a3a2
Austin Glaser <austin@boulderes.com>
2016-02-24 23:29:32 -0700

    solution

--- 004/solution.py
+++ 004/solution.py
@@ -23,25 +23,27 @@ def is_n_decimal_digits(value, n):
     return n_decimal_digits(value) == n
 
 
-def palindromes_less_than(max_value):
+def palindromes_between(min_value, max_value):
     max_digits = n_decimal_digits(max_value)
     palindromes_with_digits = [None]*max_digits
     palindromes_with_digits[0] = np.arange(0,10, 1,  dtype='i4')
     palindromes_with_digits[1] = np.arange(0,100,11, dtype='i4')
     for digits in range(3, max_digits + 1):
         seed_row = palindromes_with_digits[(digits - 2) - 1]
-        palindromes_with_digits[digits - 1] = np.zeros(len(seed_row)*9, dtype='i4')
+        palindromes_with_digits[digits - 1] = np.zeros(len(seed_row)*10, dtype='i4')
         current_row = palindromes_with_digits[digits - 1]
         i = 0
         for seed in seed_row:
             outer_ones = 10 ** (digits - 1) + 1
-            for n in range(1, 10):
+            for n in range(0, 10):
                 value = seed * 10 + n * outer_ones
                 current_row[i] = value
                 i += 1
+
     palindromes = np.concatenate(palindromes_with_digits)
-    palindromes = np.unique(palindromes[palindromes <= max_value])
-    palindromes = palindromes[1:]
+    palindromes = np.unique(palindromes)
+    palindromes = palindromes[palindromes <= max_value]
+    palindromes = palindromes[palindromes >= min_value]
     return palindromes
 
 
@@ -55,7 +57,18 @@ if __name__ == "__main__":
         raise docopt.DocoptExit("'digits' must be a positive integer")
 
     largest_number = 10**(digits) - 1
+    smallest_number = 10**(digits - 1)
     largest_product = largest_number ** 2
+    smallest_product = smallest_number ** 2
 
-    candidates = palindromes_less_than(largest_product)[::-1]
-    print candidates
+    candidates = palindromes_between(smallest_product, largest_product)[::-1]
+    for candidate in candidates:
+        factor = largest_number
+        while is_n_decimal_digits(factor, digits) and n_decimal_digits(candidate / factor) <= digits:
+            if candidate % factor == 0:
+                cofactor = candidate/factor
+                print "{p} is {f} * {c}".format(p=candidate, f=factor, c=cofactor)
+                exit(0)
+            factor -= 1
+
+    print "wut"
