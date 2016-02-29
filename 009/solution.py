e98779f5a8b17a28d76b5a4fb55f19f2dace08b7
Austin Glaser <austin@boulderes.com>
2016-02-29 14:24:46 -0700

    skeleton of problem 9

---
+++ 009/solution.py
@@ -0,0 +1,21 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+#
+# Copyright © 2016 Austin Glaser <austin@boulderes.com>
+#
+# Distributed under terms of the MIT license.
+
+"""
+Solution to project euler problem 9.
+
+Find pythagorean triplet of natural numbers a, b, and c s.t. a^2 + b^2 = c^2.
+
+Constraints:
+
+    a < b < c
+    a + b + c = 1000
+"""
+
+if __name__ == "__main__":
+    print "problem 9"
