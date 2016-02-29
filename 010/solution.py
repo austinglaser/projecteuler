6ddd82cc13d752fc4a531ccf5ab67e902ae6731d
Austin Glaser <austin@boulderes.com>
2016-02-29 14:49:35 -0700

    skeleton of problem 10

---
+++ 010/solution.py
@@ -0,0 +1,16 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+#
+# Copyright © 2016 Austin Glaser <austin@boulderes.com>
+#
+# Distributed under terms of the MIT license.
+
+"""
+Project Euler problem 10.
+
+Sum of primes < 2,000,000
+"""
+
+if __name__ == "__main__":
+    print "problem 10"
