8a55139ee2ea85675d7e87db7de1d7b167084e07
Austin Glaser <austin@boulderes.com>
2016-03-02 14:09:48 -0700

    skeleton of p11

---
+++ 011/solution.py
@@ -0,0 +1,20 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+#
+# Copyright © 2016 Austin Glaser <austin@boulderes.com>
+#
+# Distributed under terms of the MIT license.
+
+"""
+Project Euler problem 11
+
+Max adjacent product in grid
+"""
+
+import math
+import numpy as np
+
+
+if __name__ == "__main__":
+    print "problem 11"
