43c5161ca69626d7a9f3fb3364500d8c842f7223
Austin Glaser <austin@boulderes.com>
2016-03-03 23:39:32 -0700

    no need to record the ACTUAL divisors

--- 012/solution.py
+++ 012/solution.py
@@ -15,20 +15,19 @@ Triangle number factors
 
 import math
 
-def divisors(n):
-    ds = []
+def n_divisors(n):
+    ds = 0
     for d in range(1,int(math.ceil(math.sqrt(n)))):
         if n % d == 0:
-            ds.append(d)
-            ds.append(n / d)
+            ds += 2
     return ds
 
 if __name__ == "__main__":
     t = 0
     n = 1
-    d = []
-    while len(d) < 500:
+    d = 0
+    while d < 500:
         t += n
         n += 1
-        d = divisors(t)
+        d = n_divisors(t)
     print t
