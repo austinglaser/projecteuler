da07ca19ad1a83152ff786c4fc5b2799d4aff43f
Austin Glaser <austin@boulderes.com>
2016-03-04 01:18:45 -0700

    skeleton of 14

---
+++ 014/solution.py
@@ -0,0 +1,17 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+#
+# Copyright © 2016 Austin Glaser <austin@boulderes.com>
+#
+# Distributed under terms of the MIT license.
+
+"""
+Project Euler problem 14
+
+Long Sequence
+"""
+
+
+if __name__ == "__main__":
+    print "problem 14"
