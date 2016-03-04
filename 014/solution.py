cb74d9a5c315f106046fa1a7ddeeb4b4daea2d2c
Austin Glaser <austin@boulderes.com>
2016-03-04 01:27:20 -0700

    naive brute force

--- 014/solution.py
+++ 014/solution.py
@@ -12,6 +12,24 @@ Project Euler problem 14
 Long Sequence
 """
 
+def sequence_length(n):
+    s = [n]
+    s_l = 1
+    while n > 1:
+        if n % 2 == 0:
+            n /= 2
+        else:
+            n = 3*n + 1
+        s_l += 1
+    return s_l
+
 
 if __name__ == "__main__":
-    print "problem 14"
+    max_length = 0
+    max_n = 0
+    for n in range(1,1000000):
+        l = sequence_length(n)
+        if l > max_length:
+            max_length = l
+            max_n = n
+    print max_n, max_length
