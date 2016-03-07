33825e43d6389c741489464a764146ed47c1422a
Austin Glaser <austin@boulderes.com>
2016-03-07 09:58:04 -0700

    p022 solution

--- 022/solution.py
+++ 022/solution.py
@@ -7,8 +7,20 @@ Project Euler problem 22
 """
 
 
+def value(name):
+    value= 0
+    for c in name:
+        value += (ord(c) + 1) - ord('A')
+    return value
+
+
 if __name__ == "__main__":
     with open('names.txt', 'r') as namefile:
         namestr = namefile.readlines()[0]
-        names = [name.strip('"') for name in namestr.split(',')]
-    print names
+        names = sorted([name.strip('"') for name in namestr.split(',')])
+
+    totalscore = 0
+    for i, name in enumerate(names):
+        score = value(name)*(i+1)
+        totalscore += score
+    print totalscore
