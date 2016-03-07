52f64cdd97baf6c0d7392b0dd2c1ebb936a0f871
Austin Glaser <austin@boulderes.com>
2016-03-06 23:51:18 -0700

    skeleton of p21

---
+++ 021/solution.py
@@ -0,0 +1,15 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+#
+# Copyright © 2016 Austin Glaser <austin@boulderes.com>
+#
+# Distributed under terms of the MIT license.
+
+"""
+Project Euler problem 21
+"""
+
+
+if __name__ == "__main__":
+    print "problem 21"
