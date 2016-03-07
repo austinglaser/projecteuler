0b5cce870576772c04d0bf18b32fd3329127ee49
Austin Glaser <austin@boulderes.com>
2016-03-07 11:39:19 -0700

    first attempt

--- 024/solution.py
+++ 024/solution.py
@@ -10,7 +10,31 @@
 Project Euler problem 24
 """
 
+import numpy as np
+
 
 if __name__ == "__main__":
-    print 'problem 24'
+    elems = sorted(['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'])
 
+    for i in range(1000000):
+        k = None
+        for i in range(len(elems) - 2, -1, -1):
+            if elems[i] < elems[i + 1]:
+                k = i
+                break
+        if k is None:
+            break
+
+        l = None
+        for i in range(len(elems) - 1, k, -1):
+            if elems[k] < elems[i]:
+                l = i
+                break
+
+        tmp = elems[k]
+        elems[k] = elems[l]
+        elems[l] = tmp
+
+        elems[k+1:] = reversed(elems[k+1:])
+
+    print ''.join(elems)
