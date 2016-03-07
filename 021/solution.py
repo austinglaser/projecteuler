9db8b597d4c8536baa3564d73188e872b9ab0b9c
Austin Glaser <austin@boulderes.com>
2016-03-07 00:03:52 -0700

    problem 21 solution

--- 021/solution.py
+++ 021/solution.py
@@ -11,5 +11,27 @@ Project Euler problem 21
 """
 
 
+import math
+
+
+def proper_divisors(n):
+    divisors = set()
+    for i in range(1, int(math.floor(math.sqrt(n))) + 1):
+        if n % i == 0:
+            divisors.add(i)
+            divisors.add(n / i)
+    divisors.remove(n)
+    return divisors
+
+
 if __name__ == "__main__":
-    print "problem 21"
+    divisor_sums = {}
+    pair_sums = 0
+    for n in range(1,10001):
+        n_sum = sum(proper_divisors(n))
+        divisor_sums[n] = n_sum
+        if n_sum in divisor_sums:
+            if divisor_sums[n_sum] == n and n != n_sum:
+                print 'found pair', n, n_sum
+                pair_sums += n + n_sum
+    print pair_sums
