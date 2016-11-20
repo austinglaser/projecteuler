ac290f40dfac8c6ba8c9131ff146057078aa7747
Austin Glaser <austin@boulderes.com>
2016-11-19 21:05:46 -0700

    skeleton of p027

---
+++ 027/solution.py
@@ -0,0 +1,16 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+#
+# Copyright © 2016 Austin Glaser <austin@boulderes.com>
+#
+# Distributed under terms of the MIT license.
+
+"""
+Project Euler problem 27
+"""
+
+import math
+
+if __name__ == "__main__":
+    print "Problem 027"
