64a2e62716d4fd3e33658eb1f2ca384123ef873c
Austin Glaser <austin@boulderes.com>
2016-11-19 23:22:51 -0700

    start of 092

---
+++ 092/solution.c
@@ -0,0 +1,35 @@
+/**@file    solution.c
+ * @author  Austin Glaser <austin@boulderes.com>
+ * @brief   Solution Source
+ *
+ * @addtogroup SOLUTION
+ * @{
+ *
+ * @defgroup SOLUTION_PRIVATE Solution Private Members
+ * @{
+ */
+
+/* --- PRIVATE DEPENDENCIES ------------------------------------------------ */
+
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+
+/* --- PRIVATE CONSTANTS --------------------------------------------------- */
+/* --- PRIVATE DATATYPES --------------------------------------------------- */
+/* --- PRIVATE MACROS ------------------------------------------------------ */
+/* --- PRIVATE FUNCTION PROTOTYPES ----------------------------------------- */
+/* --- PUBLIC VARIABLES ---------------------------------------------------- */
+/* --- PRIVATE VARIABLES --------------------------------------------------- */
+/* --- PUBLIC FUNCTIONS ---------------------------------------------------- */
+
+int main(void)
+{
+    printf("Problem 092\n");
+}
+
+/* --- PRIVATE FUNCTION DEFINITIONS ---------------------------------------- */
+
+/** @} defgroup SOLUTION_PRIVATE */
+/** @} addtogroup SOLUTION */
