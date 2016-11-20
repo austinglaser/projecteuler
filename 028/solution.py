98333cf80db724ad2be0d99eb3bcad6f3f8d8f63
Austin Glaser <austin@boulderes.com>
2016-11-19 21:41:32 -0700

    skeleton of problem 028

---
+++ 028/solution.py
@@ -0,0 +1,17 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+#
+# Copyright © 2016 Austin Glaser <austin@boulderes.com>
+#
+# Distributed under terms of the MIT license.
+
+"""
+Project Euler problem 28
+"""
+
+import math
+import numpy as np
+
+if __name__ == "__main__":
+    print "Problem 028"
