21f50799b2d99e1b60ca65b2924286d9a29d6050
Austin Glaser <austin@boulderes.com>
2016-11-19 23:05:20 -0700

    first solution for 028

--- 028/solution.py
+++ 028/solution.py
@@ -8,10 +8,33 @@
 
 """
 Project Euler problem 28
+
+43 44 45 46 47 48 49
+42 21 22 23 24 25 26
+41 20 7  8  9  10 27
+40 19 6  1  2  11 28
+39 18 5  4  3  12 29
+38 17 16 15 14 13 30
+37 36 35 34 33 32 31
 """
 
 import math
 import numpy as np
 
+def elem(ring, diag):
+    return (2*ring + 1)**2 - 2*ring*(4 - diag)
+
+def corners(ring):
+    s = 0
+    for i in [1,2,3,4]:
+        s += elem(ring, i)
+    return s
+
+def spiral(size):
+    s = 1
+    for i in range(1,size + 1):
+        s += corners(i)
+    return s
+
 if __name__ == "__main__":
-    print "Problem 028"
+    print spiral(500)
